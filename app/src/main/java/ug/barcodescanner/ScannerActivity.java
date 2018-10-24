3741882d9b1a531638683d1dabab3e2592927066
Dennis Bilson <quabynahdennis@gmail.com>
2018-10-24 10:50:58 +0000

    Initial commit

---
+++ app/src/main/java/ug/barcodescanner/ScannerActivity.java
@@ -0,0 +1,17 @@
+package ug.barcodescanner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * {@link ScannerActivity} is responsible for scanning texts from an image source and displaying it in an image view
+ * This scanned text will then be processed and decoded by the firebase ML Barcode Kit to retrieve any barcode available
+ */
+public class ScannerActivity extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_scanner);
+	}
+}
