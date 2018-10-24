dd4a03b527c6090c4a1b96dbb959996c675026ea
Dennis Bilson <quabynahdennis@gmail.com>
2018-10-24 11:09:28 +0000

    Added permissions to the android manifest file
    Enabled Firebase Application in ScannerActivity

--- app/src/main/java/ug/barcodescanner/ScannerActivity.java
+++ app/src/main/java/ug/barcodescanner/ScannerActivity.java
@@ -3,6 +3,8 @@ package ug.barcodescanner;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.FirebaseApp;
+
 /**
  * {@link ScannerActivity} is responsible for scanning texts from an image source and displaying it in an image view
  * This scanned text will then be processed and decoded by the firebase ML Barcode Kit to retrieve any barcode available
@@ -13,5 +15,10 @@ public class ScannerActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_scanner);
+		
+		//Firebase initialization
+		FirebaseApp.initializeApp(this);
+		
+		
 	}
 }
